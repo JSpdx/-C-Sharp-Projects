5ca734332762fe8828ee6487c7baf42601b3799d
JS_PDX <57430576+DrFunkenstein@users.noreply.github.com>
2020-04-02 10:11:43 -0700

    add Guid

--- BlackjackApp/BlackjackApp/Program.cs
+++ BlackjackApp/BlackjackApp/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using Casino;
+using Casino.TwentyOne;
 
 namespace BlackjackApp
 {
@@ -11,7 +13,10 @@ namespace BlackjackApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name.");
+            const string casinoName = "Grand Hotel and Casino";
+
+
+            Console.WriteLine("Welcome to the {0}. Let's start by telling me your name.", casinoName);
             string playerName = Console.ReadLine();
             Console.WriteLine("And how much money did you bring today?");
             int bank = Convert.ToInt32(Console.ReadLine());
@@ -20,6 +25,11 @@ namespace BlackjackApp
             if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
             {
                 Player player = new Player(playerName, bank);
+                player.Id = Guid.NewGuid();
+                using (StreamWriter file = new StreamWriter(@"C:\Users\Jon\Logs\log.txt", true))
+                {
+                    file.WriteLine(player.Id);
+                }
                 Game game = new TwentyOneGame();
                 game += player;
                 player.IsActivelyPlaying = true;
--- BlackjackApp/Casino/Player.cs
+++ BlackjackApp/Casino/Player.cs
@@ -4,10 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BlackjackApp
+namespace Casino
 {
     public class Player
     {
+        public Player(string name) : this(name, 100)
+        {
+        }
         public Player(string name, int beginningBalance)
         {
             Hand = new List<Card>();
@@ -20,6 +23,7 @@ namespace BlackjackApp
         public string Name { get; set; }
         public bool IsActivelyPlaying { get; set; }
         public bool Stay { get; set; }
+        public Guid Id { get; set; }
 
         public bool Bet(int amount)        // subtracts the bet from the player's balance, and returns true of false if it was successful
         {
