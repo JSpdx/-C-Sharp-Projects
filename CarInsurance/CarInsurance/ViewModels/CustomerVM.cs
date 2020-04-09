caadc4f36fa220a2847a6e5e5d3bc5a45729c369
JS_PDX <57430576+DrFunkenstein@users.noreply.github.com>
2020-04-09 10:20:42 -0700

    add customer viewmodel

---
+++ CarInsurance/CarInsurance/ViewModels/CustomerVM.cs
@@ -0,0 +1,20 @@
+﻿using CarInsurance.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarInsurance.ViewModels
+{
+    public class CustomerVM
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Nullable<decimal> Quote { get; set; }
+        public string Coverage { get; set; }
+
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Car> Cars { get; set; }
+    }
+}
